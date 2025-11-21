519156b0cf68565c869eb9be2bbb4116e8040c20
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2025-11-21 16:02:06 -0500

    modificacion

--- infra/key-vault.tf
+++ infra/key-vault.tf
@@ -18,9 +18,6 @@ resource "azurerm_key_vault" "keyvault" {
   location                    = data.azurerm_resource_group.rg.location
   resource_group_name         = data.azurerm_resource_group.rg.name
   tenant_id                   = var.tenant_id
-
-  enabled_for_disk_encryption = true
-  soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   enable_rbac_authorization   = true
 
